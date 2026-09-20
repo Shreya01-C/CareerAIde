e9c62d131d2ba857df471072b1e4c92a067b89c9
Shreya01-C <shreya004r@gmail.com>
2026-09-20 20:53:51 +0530

    Update LandingPage.jsx

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -38,7 +38,7 @@ const LandingPage = () => {
               <LayoutTemplate className={landingPageStyles.logoIconInner} />
             </div>
             <span className={landingPageStyles.logoText}>
-              ResumeXpert
+              ResumeFello
             </span>
           </div>
 
@@ -237,7 +237,7 @@ const LandingPage = () => {
           <div className={landingPageStyles.featuresContainer}>
             <div className={landingPageStyles.featuresHeader}>
               <h2 className={landingPageStyles.featuresTitle}>
-                Why Choose <span className={landingPageStyles.featuresTitleGradient}>ResumeXpert?</span>
+                Why Choose <span className={landingPageStyles.featuresTitleGradient}>CareerAIde?</span>
               </h2>
               <p className={landingPageStyles.featuresDescription}>
                 Everything you need to create a professional resume that stands out
@@ -314,7 +314,7 @@ const LandingPage = () => {
           <p className={landingPageStyles.footerText}>
             Crafted  <span className={landingPageStyles.footerHeart}></span> by{' '}
             <a href="https://portfolio-4vlx.vercel.app/" target="_blank" className={landingPageStyles.footerLink}>
-            Anurag 😃
+            Shreya
             </a>
           </p>
         </div>
